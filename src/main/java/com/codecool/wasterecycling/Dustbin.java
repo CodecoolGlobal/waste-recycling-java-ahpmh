40cbb3e4381a524a24e3acfec76b25b5f394bb75
ahpmh <alwayshappypeople@gmail.com>
2022-11-28 23:02:35 +0100

    add Well-functioning Dustbin

--- src/main/java/com/codecool/wasterecycling/Dustbin.java
+++ src/main/java/com/codecool/wasterecycling/Dustbin.java
@@ -1,4 +1,61 @@
 package com.codecool.wasterecycling;
 
+import java.util.ArrayList;
+
 public class Dustbin {
+        private String color;
+        private ArrayList<String> plastic = new ArrayList<>();
+        private ArrayList<String> paper = new ArrayList<>();
+        private ArrayList<String> houseWaste = new ArrayList<>();
+
+
+        public Dustbin(String color){
+                this.color = color;
+        }
+
+        public String getColor(){
+                return color;
+        }
+
+
+
+        public void throwOutGarbage(Garbage garbage) {
+                if (garbage instanceof PlasticGarbage) {
+                        plastic.add(garbage.getGarbage());
+                } else if (garbage instanceof PaperGarbage) {
+                        paper.add(garbage.getGarbage());
+                } else {
+                        houseWaste.add(garbage.getGarbage());
+                }
+        }
+
+        public int getHouseWasteCount() {
+                return houseWaste.size();
+        }
+
+        public int getPaperCount() {
+                return paper.size();
+        }
+
+        public int getPlasticCount() {
+                return plastic.size();
+        }
+
+
+        public void emptyContents(){
+                plastic.clear();
+                paper.clear();
+                houseWaste.clear();
+        }
+
+
+        public String toString(){
+
+                return null;
+        }
+
+        private void displayContents(){
+                System.out.println();
+        }
+
 }

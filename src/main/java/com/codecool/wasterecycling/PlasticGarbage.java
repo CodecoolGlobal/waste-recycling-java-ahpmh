4c4820ba797515ae531d3d60018d6727b8542ddb
ahpmh <alwayshappypeople@gmail.com>
2022-11-28 23:24:47 +0100

    add plastic garbage

--- src/main/java/com/codecool/wasterecycling/PlasticGarbage.java
+++ src/main/java/com/codecool/wasterecycling/PlasticGarbage.java
@@ -1,4 +1,25 @@
 package com.codecool.wasterecycling;
 
 public class PlasticGarbage extends Garbage {
+
+        private String cleanFlag;
+
+        public PlasticGarbage(String garbageName, String cleanFlag) {
+                super(garbageName);
+                this.cleanFlag = isClean(cleanFlag);
+        }
+
+        public String isClean(String cleanFlag) {
+                if (!cleanFlag.equals("clean")) {
+                        return clean();
+                }
+                return cleanFlag;
+        }
+
+        public String clean() {
+                        return "clean";
+                }
+        }
+
+
 }

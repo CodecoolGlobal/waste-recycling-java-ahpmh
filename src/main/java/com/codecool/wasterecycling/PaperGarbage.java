b68a54d4720d79931435aa5c32a89d60453de7af
ahpmh <alwayshappypeople@gmail.com>
2022-11-28 23:31:29 +0100

    add PaperGarbage

--- src/main/java/com/codecool/wasterecycling/PaperGarbage.java
+++ src/main/java/com/codecool/wasterecycling/PaperGarbage.java
@@ -1,4 +1,21 @@
 package com.codecool.wasterecycling;
 
 public class PaperGarbage extends Garbage {
+        private String squeezeFlag;
+
+        public PaperGarbage(String garbageName, String squeezeFlag) {
+                super(garbageName);
+                this.squeezeFlag = isSqueezed(squeezeFlag);
+        }
+
+        public String isSqueezed(String cleanFlag) {
+                if (!cleanFlag.equals("squeeze")) {
+                        return squeeze();
+                }
+                return squeezeFlag;
+        }
+
+        public String squeeze() {
+                return "squeezed";
+        }
 }

2039a8c162143515957b14a313c51511f91dfc40
ahpmh <alwayshappypeople@gmail.com>
2022-11-28 23:49:29 +0100

    prepared instances, will continue from here( the logic)

--- src/main/java/com/codecool/wasterecycling/Main.java
+++ src/main/java/com/codecool/wasterecycling/Main.java
@@ -3,5 +3,13 @@ package com.codecool.wasterecycling;
 public class Main {
 
     public static void main(String[] args) {
+            Dustbin Plastico = new Dustbin("amarillo");
+            Dustbin Vidrio = new Dustbin("verde");
+            Dustbin Papel = new Dustbin("azul");
+
+            Garbage PlasticCup = new PlasticGarbage("PlasticCup", "dirty");
+            Garbage PlasticBottle = new PlasticGarbage("PlasticBottle", "clean");
+
+
     }
 }
